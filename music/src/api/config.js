3fdccfb9911234866f17760a0866561c98718d67
Isaac Batista <daniel.bautista@pengostores.com>
2018-04-24 10:56:09 -0500

    conectando api lastfm

---
+++ music/src/api/config.js
@@ -0,0 +1,8 @@
+'use strict'
+
+export default {
+    appName: 'music test',
+    apiKey: '25b8a8e90c69b3f0af331157c5234bbd',
+    secret: '44d9a2c109cc5fdee2e4d853641df960',
+    registeredTo: 'isaac-batista'
+}
